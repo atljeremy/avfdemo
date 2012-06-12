ac6e79069358f73fcc8fdfc1f45922f6838f6416
atljeremy <atljeremy@me.com>
2012-06-12 19:00:58 -0400

    Adding Photo functionality

--- www/js/avf-demo-main.js
+++ www/js/avf-demo-main.js
@@ -1,44 +1,121 @@
-// Geolocation onSuccess Callback
-//
-var onSuccess = function(position) {
-        var geolocationDiv = $('#geolocationDiv');
-        geolocationDiv.append( 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
-                            'Altitude: '           + position.coords.altitude              + '<br />' +
-                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-                            'Heading: '            + position.coords.heading               + '<br />' +
-                            'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + new Date(position.timestamp)          + '<br />' +
-                            '<hr>');
-};
+//*********************************************************************
+// AUTHOR: Jeremy Fox
+// COURSE: Advanced Visual Frameworks
+// FOR: Full Sail Online
+// TERM: 1206
+//*********************************************************************
 
-// Geolocation onError Callback receives a PositionError object
-//
-var onError = function(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+//*********************************************************************
+// CONTACTS
+//*********************************************************************
+var contactsBtn = $('#notification');
+contactsBtn.live('live', function(){
+    
+});
+
+//*********************************************************************
+// CAMERAS/PHOTOS
+//*********************************************************************
+var pictureSource;
+var destinationType;
+
+$('#photoPage').live('pageshow', function(event, ui){
+    alert("PHOTOS PAGE IS READY!!!!!!!!!!!!!");
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+});
+
+function onPhotoDataSuccess(imageData) {
+    
+    var smallImage = document.getElementById('smallImage');
+    smallImage.style.display = 'block';
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+    
+function onPhotoURISuccess(imageURI) {
+    
+    var largeImage = document.getElementById('largeImage');
+    largeImage.style.display = 'block';
+    largeImage.src = imageURI;
 }
 
-// Get References To Buttons
-//
+var capturePhoto = function() {
+    alert("CAPTURE PHOTO");
+    navigator.camera.getPicture(onPhotoDataSuccess, photosOnFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+}
+
+var capturePhotoEdit = function() {
+    alert("CAPTURE PHOTO EDIT");
+    navigator.camera.getPicture(onPhotoDataSuccess, photosOnFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+}
+
+var getPhoto = function(source) {
+    alert("GET PHOTO + SOURCE: " + source);
+    navigator.camera.getPicture(onPhotoURISuccess, photosOnFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+}
+
+var photosOnFail = function(message) {
+    alert('Failed because: ' + message);
+}
+
+$("#capturePhoto")    .live('click', capturePhoto);
+
+$("#capturePhotoEdit").live('click', capturePhotoEdit);
+
+$("#getPhotoLibrary") .live('click', function(){
+    getPhoto(pictureSource.PHOTOLIBRARY);
+});
+
+$("#getPhotoAlbum")   .live('click', function(){
+    getPhoto(pictureSource.SAVEDPHOTOALBUM);
+});
+    
+//*********************************************************************
+// NOTIFICATIONS
+//*********************************************************************
 var notificationBtn = $('#notification');
-var geolocationBtn = $('#geolocation');
 
 notificationBtn.live('click', function(){
-  function alertDismissed() {
-      // do nothing
-      console.log("Dismissed");
-  }
-  
-  navigator.notification.alert(
-            'You are the winner!',  // message
-            alertDismissed(),         // callback
-            'Game Over',            // title
-            'Done'                  // buttonName
-        );
+    function alertDismissed() {
+        // do nothing
+        console.log("Dismissed");
+    }
+    
+    navigator.notification.alert(
+    'You are the winner!',  // message
+    alertDismissed(),         // callback
+    'Game Over',            // title
+    'Done'                  // buttonName
+    );
 });
-  
+
+//*********************************************************************
+// GEOLOCATION
+//*********************************************************************
+var geolocationBtn = $('#geolocation');
+
+var geoOnSuccess = function(position) {
+    var geolocationDiv = $('#geolocationDiv');
+    geolocationDiv.append( 'Latitude: '           + position.coords.latitude              + '<br />' +
+    'Longitude: '          + position.coords.longitude             + '<br />' +
+    'Altitude: '           + position.coords.altitude              + '<br />' +
+    'Accuracy: '           + position.coords.accuracy              + '<br />' +
+    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+    'Heading: '            + position.coords.heading               + '<br />' +
+    'Speed: '              + position.coords.speed                 + '<br />' +
+    'Timestamp: '          + new Date(position.timestamp)          + '<br />' +
+    '<hr>');
+};
+
+var geoOnError = function(error) {
+    alert('code: ' + error.code    + '\n' +
+    'message: ' + error.message + '\n');
+}
+
 geolocationBtn.live('click', function(){
-  navigator.geolocation.getCurrentPosition(onSuccess, onError, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+    navigator.geolocation.getCurrentPosition(geoOnSuccess, geoOnError, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
 });
